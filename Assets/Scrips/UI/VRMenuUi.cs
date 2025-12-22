db2fac4f635948ff8265f940bad3e7b63744bada
rndp\rndp10 <jungkim1228@gmail.com>
2025-12-22 09:49:59 +0900

    fix : ajust hierarchy object position

--- Assets/Scrips/UI/VRMenuUi.cs
+++ Assets/Scrips/UI/VRMenuUi.cs
@@ -323,7 +323,7 @@ public class VRMenuUI : MonoBehaviour
         
         // Nombre de joueurs
         if (currentPlayerCountText != null)
-            currentPlayerCountText.text = $"Joueurs: {VRRoomManager.Instance.PlayerCount}";
+            currentPlayerCountText.text = $"\nPlayers: {VRRoomManager.Instance.PlayerCount}";
         
         // Liste des joueurs
         RefreshPlayerList();
